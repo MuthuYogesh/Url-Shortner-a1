66ee0655091af71b96ea6c7c035441f3a1a2c3c9
MuthuYogesh <muthuyogeshb@gmail.com>
2025-07-30 23:57:16 +0530

    Redis setup updated with upstash for deployment and local redis with docker container for production

--- backend/config.mjs
+++ backend/config.mjs
@@ -5,5 +5,7 @@ const PORT = process.env.PORT;
 const URI = process.env.MongoDB;
 const feUrlDev = process.env.FRONTEND_URL_DEV;
 const feUrlProd = process.env.FRONTEND_URL_PROD;
+const redisUrl = process.env.UPSTASH_REDIS_URL;
+const nodeEnv = process.env.NODE_ENV;
 
-export { PORT, URI, feUrlDev, feUrlProd };
+export { PORT, URI, feUrlDev, feUrlProd, redisUrl, nodeEnv };
--- backend/src/redis/redisClient.mjs
+++ backend/src/redis/redisClient.mjs
@@ -1,10 +1,19 @@
 import { createClient } from "redis";
+import { nodeEnv, redisUrl } from "../../config.mjs";
 
+const isProd = nodeEnv === 'production';
+const redisURL = isProd ? redisUrl : "redis://127.0.0.1:6379";
 // creates instance with default settings. Redis Default PORT localhost:6379
-const redisClient = createClient();
+const redisClient = createClient({
+    url: redisURL,
+    socket: isProd ? {
+        tls: true, // Transport Layer Security: Secure communication b/w server and redis server
+        rejectUnauthorized: false
+    } : {} // Local Docker containerized Redis DB does not require TLS
+});
 
 // adding event listener to catch errors
-redisClient.on('error', (err)=>console.error('redis client error: ',err));
+redisClient.on('error', (err) => console.error('redis client error: ', err));
 
 // in ES module mjs top level await is available with out Async function
 await redisClient.connect();
