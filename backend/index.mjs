13b1278e694cf8019f28f1d18392e67345e2345b
MuthuYogesh <muthuyogeshb@gmail.com>
2025-07-31 00:28:19 +0530

    app.options('*', cors()) incompatable with express 5+ fixed with/.*/

--- backend/index.mjs
+++ backend/index.mjs
@@ -11,23 +11,27 @@ app.use(express.json());
 const allowedOrigins = [feUrlDev, feUrlProd];
 
 // It allows Cross Origin Requests
-app.use(cors(
-    cors({
-        origin: (origin, callback) => {
-            // Allow requests with no origin (like mobile apps or curl)
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            } else {
-                callback(new Error("CORS not allowed"));
-            }
-        },
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allow preflight
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-));
+app.use(cors({
+    origin: (origin, callback) => {
+        // Allow requests with no origin (like mobile apps or curl)
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS not allowed"));
+        }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // allow preflight
+    allowedHeaders: ["Content-Type", "Authorization"],
+})
+);
 
-app.options("*", cors());
+// console.log("Problem::<1>")
+
+app.options(/.*/, cors());  // app.options("*", cors());  works in Express 4 not in 5+
+
+// console.log("Problem::<2>")
+
 
 mongoose.connect(URI)
     .then(() => console.log("Database connected successfully!"))
--- backend/src/redis/redisClient.mjs
+++ backend/src/redis/redisClient.mjs
@@ -2,6 +2,7 @@ import { createClient } from "redis";
 import { nodeEnv, redisUrl } from "../../config.mjs";
 
 const isProd = nodeEnv === 'production';
+// console.log(isProd, redisUrl);
 const redisURL = isProd ? redisUrl : "redis://127.0.0.1:6379";
 // creates instance with default settings. Redis Default PORT localhost:6379
 const redisClient = createClient({
@@ -12,11 +13,13 @@ const redisClient = createClient({
     } : {} // Local Docker containerized Redis DB does not require TLS
 });
 
+
 // adding event listener to catch errors
 redisClient.on('error', (err) => console.error('redis client error: ', err));
 
 // in ES module mjs top level await is available with out Async function
 await redisClient.connect();
+
 
 // Export
 export default redisClient;
